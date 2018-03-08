1b9aa68629044abff267b103755db61e08ba45a5
Gregor Robertson <gregor_r@btinternet.com>
2018-03-08 12:55:47 +0000

    saving progress before working on mqn

---
+++ hierarchical-deep-RL-iclr16_basicsubgoal/dqn/grgr_additions/model/init.lua
@@ -0,0 +1,41 @@
+require 'nn'
+require 'nngraph'
+require 'grgr_additions.LinearNB'
+require 'grgr_additions.model.dqn'
+require 'grgr_additions.model.drqn'
+require 'grgr_additions.model.mqn'
+require 'grgr_additions.model.rmqn'
+require 'grgr_additions.model.frmqn'
+
+function g_create_network(args)
+    args.hist_len           = args.hist_len or 10
+    args.name               = "dqn"
+    args.n_actions          = args.n_actions or 6
+    args.ncols              = args.ncols or 3
+    args.image_dims         = args.image_dims or {3, 32, 32}
+    args.input_dims         = args.input_dims or {args.hist_len * args.ncols, 32, 32}
+    args.n_units            = args.n_units or {32, 64}
+    args.pad                = args.pad or {1, 1}
+    args.n_hid_enc          = args.n_hid_enc or 256
+    args.edim               = args.edim or 256
+    args.memsize            = args.memsize or (args.hist_len - 1)
+    args.lindim             = args.lindim or args.edim / 2
+    args.lstm_dim           = args.edim or 256
+    args.gpu                = args.gpu or -1
+    args.conv_dim           = args.n_units[#args.n_units] * 8 * 8
+    args.Linear             = nn.LinearNB
+
+    if args.name == "dqn" then
+        return DQN.new(args)
+    elseif args.name == "drqn" then
+        return DRQN.new(args)
+    elseif args.name == "mqn" then
+        return MQN.new(args)
+    elseif args.name == "rmqn" then
+        return RMQN.new(args)
+    elseif args.name == "frmqn" then
+        return FRMQN.new(args)
+    else
+        error("Invalid model name:" .. args.name)
+    end
+end
---
+++ hierarchical-deep-RL-iclr16_basicsubgoal/dqn/grgr_additions/model/net.lua
@@ -0,0 +1,105 @@
+require 'cudnn'
+local Net = torch.class('Net')
+function Net:__init(args)
+    self.args = args
+    self.share_list = {}
+    self.init_states = self:build_init_states(args)
+    self:reset_init_states()
+    self.recurrent = #self.init_states > 0
+    self.net = self:build_model(args)
+
+    if args.gpu > 0 then
+        self:cuda()
+    end
+    -- IMPORTANT! do weight sharing after model is in cuda
+    for k, v in pairs(self.share_list) do
+        local m1 = v[1].data.module
+        for j = 2,#v do
+            local m2 = v[j].data.module
+            m2:share(m1,'weight','bias','gradWeight','gradBias')
+        end
+    end
+end
+
+function Net:build_init_states(args)
+    return {}
+end
+
+function Net:reset_init_states(batch_size)
+    for j=1,#self.init_states do
+        local size = self.init_states[j]:size()
+        size[1] = batch_size or 1
+        self.init_states[j]:resize(size)
+        self.init_states[j]:fill(0)
+    end
+end
+
+function Net:forward(x)
+    if self.recurrent then
+        local input = {x}
+        self:reset_init_states(x:size(1))
+        for i = 1, #self.init_states do
+            table.insert(input, self.init_states[i])
+        end
+        return self.net:forward(input)
+    else
+        return self.net:forward(x)
+    end
+end
+
+function Net:backward(x, gradOutput)
+    if self.recurrent then
+        local input = {x}
+        for i = 1, #self.init_states do
+            table.insert(input, self.init_states[i])
+        end
+        return self.net:backward(input, gradOutput)
+    else
+        return self.net:backward(x, gradOutput)
+    end
+end
+
+function Net:getParameters()
+    return self.net:getParameters()
+end
+
+function Net:clone()
+    local clone = g_create_network(self.args)
+    clone.net = self.net:clone()
+    return clone
+end
+
+function Net:cuda()
+    self.net = self.net:cuda()
+    cudnn.convert(self.net, cudnn)
+    for i=1,#self.init_states do
+        self.init_states[i] = self.init_states[i]:cuda()
+    end
+    return self
+end
+
+function Net:float()
+    self.net = self.net:float()
+    for i=1,#self.init_states do
+        self.init_states[i] = self.init_states[i]:float()
+    end
+    return self
+end
+
+function Net:training()
+    self.net:training()
+    return self
+end
+
+function Net:evaluate()
+    self.net:evaluate()
+    return self
+end
+
+function Net:share_module(name, node)
+    if self.share_list[name] == nil then
+        self.share_list[name] = {node}
+    else
+        table.insert(self.share_list[name], node)
+    end
+end
